f58fa1980a8a5dd59648ebec57e36e08af1af867
Filip Jonckers <filip@jonckers.org>
2021-01-28 22:40:04 +0100

    disable json files to avoid using the memory card too much

--- dump1090fa/start.sh
+++ dump1090fa/start.sh
@@ -2,4 +2,5 @@
 echo Starting webserver
 service lighttpd start
 echo Starting dump1090-fa ....
-dump1090-fa --write-json /run/dump1090-fa --net --quiet --fix --lat $DUMP_LAT --lon $DUMP_LON 
+# dump1090-fa --write-json /run/dump1090-fa --net --quiet --fix --lat $DUMP_LAT --lon $DUMP_LON 
+dump1090-fa --net --quiet --fix --aggressive --lat $DUMP_LAT --lon $DUMP_LON 
