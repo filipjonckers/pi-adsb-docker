c1ad8bbaf811b73737bae812a9d9caf63d1d87ef
Filip Jonckers <filip@jonckers.org>
2021-01-29 15:40:22 +0000

    enable json output keeping in mind memcard wear, improve biterror fixing

--- dump1090fa/start.sh
+++ dump1090fa/start.sh
@@ -2,5 +2,4 @@
 echo Starting webserver
 service lighttpd start
 echo Starting dump1090-fa ....
-# dump1090-fa --write-json /run/dump1090-fa --net --quiet --fix --lat $DUMP_LAT --lon $DUMP_LON 
-dump1090-fa --net --quiet --fix --aggressive --lat $DUMP_LAT --lon $DUMP_LON 
+dump1090-fa --write-json /run/dump1090-fa --write-json-every 30 --net --quiet --fix --fix-2bit --modeac --lat $DUMP_LAT --lon $DUMP_LON 
